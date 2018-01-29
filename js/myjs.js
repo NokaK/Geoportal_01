b55f340610b83f6462c0f721f89bffbab098c6f4
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-01-29 16:58:56 +0400

    blog page
    add blog page

--- js/myjs.js
+++ js/myjs.js
@@ -80,39 +80,39 @@ $(document).on('ready', function() {
 // });
 
 
-$(document).ready(function() {
-    // Configure/customize these variables.
-    var showChar = 400;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "ვრცლად ... ";
-    var lesstext = "...უკან დაბრუნება";
+// $(document).ready(function() {
+//     // Configure/customize these variables.
+//     var showChar = 400;  // How many characters are shown by default
+//     var ellipsestext = "...";
+//     var moretext = "ვრცლად ... ";
+//     var lesstext = "... უკან დაბრუნება";
     
 
-    $('.more').each(function() {
-        var content = $(this).html();
+//     $('.more').each(function() {
+//         var content = $(this).html();
  
-        if(content.length > showChar) {
+//         if(content.length > showChar) {
  
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
+//             var c = content.substr(0, showChar);
+//             var h = content.substr(showChar, content.length - showChar);
  
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+//             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
  
-            $(this).html(html);
-        }
+//             $(this).html(html);
+//         }
  
-    });
+//     });
  
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
-});
+//     $(".morelink").click(function(){
+//         if($(this).hasClass("less")) {
+//             $(this).removeClass("less");
+//             $(this).html(moretext);
+//         } else {
+//             $(this).addClass("less");
+//             $(this).html(lesstext);
+//         }
+//         $(this).parent().prev().toggle();
+//         $(this).prev().toggle();
+//         return false;
+//     });
+// });
