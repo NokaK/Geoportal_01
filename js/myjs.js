a4c112da55fc363f62aa6a727acedc076ce535cf
NokaK <nona.kipiani07b@geolab.edu.ge>
2018-02-02 17:49:34 +0400

    changed
    changed

--- js/myjs.js
+++ js/myjs.js
@@ -35,8 +35,25 @@ $(document).on('ready', function() {
         dots: true,
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive:[
+          {
+            breakpoint: 1024,
+            settings:{
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 740,
+            settings:{
+              slidesToShow:1,
+              slidesToScroll:1
+            }
+          }
+        ]
       });
+
       $(".center").slick({
         dots: true,
         infinite: true,
@@ -56,78 +73,12 @@ $(document).on('ready', function() {
       });
     });
 
-$(function(){
-
-        $('#search').click(function() {
-          var clicks = $(this).data('clicks');
-          if (clicks) {
-            closeNav()
-          } else {
-             openNav()
-          }
-          $(this).data("clicks", !clicks);
-        });
-
-    
-
-        });
-
-// $(window).resize(function(){
-
-//        if ($(window).width() <= 991) {  
-//         $(".regular").slick({
-//         dots: true,
-//         infinite: true,
-//         slidesToShow: 2,
-//         slidesToScroll: 1
-//       });
-
-//        }    
-//        else {
-//         $(".regular").slick({
-//         dots: true,
-//         infinite: true,
-//         slidesToShow: 4,
-//         slidesToScroll: 1
-//       });
-//        } 
-
-// });
-
-
-// $(document).ready(function() {
-//     // Configure/customize these variables.
-//     var showChar = 400;  // How many characters are shown by default
-//     var ellipsestext = "...";
-//     var moretext = "ვრცლად ... ";
-//     var lesstext = "... უკან დაბრუნება";
-    
-
-//     $('.more').each(function() {
-//         var content = $(this).html();
  
-//         if(content.length > showChar) {
- 
-//             var c = content.substr(0, showChar);
-//             var h = content.substr(showChar, content.length - showChar);
- 
-//             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
-//             $(this).html(html);
-//         }
- 
-//     });
- 
-//     $(".morelink").click(function(){
-//         if($(this).hasClass("less")) {
-//             $(this).removeClass("less");
-//             $(this).html(moretext);
-//         } else {
-//             $(this).addClass("less");
-//             $(this).html(lesstext);
-//         }
-//         $(this).parent().prev().toggle();
-//         $(this).prev().toggle();
-//         return false;
-//     });
-// });
+  $('.search-icon').click(function(){
+    $('.search').toggle();
+  })
+
+
+
+
+
